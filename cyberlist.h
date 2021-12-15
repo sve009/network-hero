8b61332b75c6747d9d00f5ddb2f34bb05babb95c
Anickeson <67401309+Anickeson@users.noreply.github.com>
2021-12-14 20:39:36 -0600

    Added some cybers and moves

--- cyberlist.h
+++ cyberlist.h
@@ -1,8 +1,10 @@
 #ifndef CYBERLIST
 #define CYBERLIST
 static int num_cybers = 3;
-char* moves0[] = {"blah", "blab", "ha", "oh no"};
-char* moves1[] = {"slap", "kick", "fall", "bite"};
-char* moves2[] = {"overaggressive high five", "water bottle flip", "vape", "drink"};
-cyber_t cybers[] = {{"Bob", 0, 0, 50, 10, 10, 10, 10, 10, 10, 0, moves0}, {"Bill", 0, 0, 50, 10, 10, 10, 10, 10, 10, 1, moves1}, {"Brad", 0, 0, 100, 10, 10, 10, 10, 10, 10, 2, moves2}};
+char* moves0[] = {"fireball", "kick", "rally", "barbeque"};
+char* moves1[] = {"lightning bolt", "thunder gun", "tail whip", "bite"};
+char* moves2[] = {"rock slide", "crush", "slam", "harden"};
+char* moves3[] = {"water pistol", "freeze", "ice Armor", "slam"};
+char* moves4[] = {"roost", "intimidate", "aeroblast", "peck"};
+cyber_t cybers[] = {{"Cyndakwill", -1, 0, 80, 30, 20, 20, 30, 30, 10, 0, moves0}, {"Peekahchew", -1, 0, 80, 40, 15, 30, 10, 30, 10, 3, moves1}, {"Geoguy", -1, 0, 140, 20, 40, 10, 40, 15, 30, 2, moves2}, {"Pipluhp", -1, 0, 80, 30, 30, 15, 30, 30, 30, 1, moves3}, {"Ho-Oh-No", -1, 0, 60, 25, 30, 20, 20, 40, 20, 4, moves4}};
 #endif
