52d2d107f127d2069b83f27aa333a922b1e84820
anickeson <andrew.nickeson@gmail.com>
2021-12-15 13:53:26 -0600

    added a new cyber

--- cyberlist.h
+++ cyberlist.h
@@ -11,6 +11,7 @@ char* moves6[] = {"fireball", "pyrosplosion", "charge", "rock slide"};
 char* moves7[] = {"water pistol", "wave", "bite", "puff"};
 char* moves8[] = {"slash", "heavy slash", "pound", "harden"};
 char* moves9[] = {"peck", "plague", "torrent", "roost"};
+char* moves10[] = {"gun", "knife", "pen", "sword"};
 cyber_t cybers[] = {{"Cyndakwill", -1, 0, 80, 30, 20, 20, 30, 30, 10, 0, moves0},
        {"Peekahchew", -1, 0, 80, 40, 15, 30, 10, 30, 10, 3, moves1},
        {"Geoguy", -1, 0, 140, 20, 40, 10, 40, 15, 30, 2, moves2},
@@ -20,5 +21,6 @@ cyber_t cybers[] = {{"Cyndakwill", -1, 0, 80, 30, 20, 20, 30, 30, 10, 0, moves0}
        {"Infiirno", -1, 0, 105, 35, 5, 10, 10, 10, 20, 0, moves6},
        {"Fiiish", -1, 0, 70, 20, 15, 20, 20, 40, 20, 1, moves7},
        {"Stoneknife", -1, 0, 80, 20, 20, 20, 20, 30, 10, 2, moves8},
-       {"Ravyn", -1, 0, 55, 20, 10, 20, 15, 50, 20, 4, moves9}};
+       {"Ravyn", -1, 0, 55, 20, 10, 20, 15, 50, 20, 4, moves9},
+       {"Regular guy", -1, 0, 80, 20, 20, 20, 20, 20, 20, 0, moves10}};
 #endif
